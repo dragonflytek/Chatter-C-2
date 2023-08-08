ae16253376f3bb0d0ea97e9271894a6af16c3e09
dragonflytek <106543092+dragonflytek@users.noreply.github.com>
2023-08-08 10:06:33 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDXINZQvXaqkK8gjKxz8EqKOqxgIJZIjfU",
+    authDomain: "chatter-2-8b89d.firebaseapp.com",
+    projectId: "chatter-2-8b89d",
+    storageBucket: "chatter-2-8b89d.appspot.com",
+    messagingSenderId: "424257575774",
+    appId: "1:424257575774:web:5b26267a98e3702984cdcf"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
